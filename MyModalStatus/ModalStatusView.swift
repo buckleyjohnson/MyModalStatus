9c53873e59805721728626e5278b64f0551d552f
Buckley Johnson <buck@swingu.com>
2019-12-18 19:17:58 -0700

    Initial Commit

---
+++ MyModalStatus/ModalStatusView.swift
@@ -0,0 +1,19 @@
+//
+//  ModalStatusView.swift
+//  MyModalStatus
+//
+//  Created by buckley johnson on 12/18/19.
+//  Copyright © 2019 SwingU. All rights reserved.
+//
+
+import UIKit
+
+class ModalStatusView: UIView {
+
+ 
+    
+    @IBOutlet weak var statusImage: UIImageView!
+    
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subHeadlineLabel: UILabel!
+}
---
+++ MyModalStatus/MyModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  MyModalStatus.h
+//  MyModalStatus
+//
+//  Created by buckley johnson on 12/18/19.
+//  Copyright © 2019 SwingU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyModalStatus.
+FOUNDATION_EXPORT double MyModalStatusVersionNumber;
+
+//! Project version string for MyModalStatus.
+FOUNDATION_EXPORT const unsigned char MyModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyModalStatus/PublicHeader.h>
+
+
